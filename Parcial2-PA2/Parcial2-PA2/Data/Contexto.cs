f59947c581813cda3956129b833f9af9f9023107
Leonardo Emil <48894730+lean56@users.noreply.github.com>
2020-03-11 15:08:29 -0400

    creando contexto

---
+++ Parcial2-PA2/Parcial2-PA2/Data/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2_PA2.Data
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source= Database/Parcial2DB.db");
+        }
+    }
+}
